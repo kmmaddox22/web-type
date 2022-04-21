3e361a088bea8347fb7c87024ecac094ee3d34ce
kmmaddox22 <99916355+kmmaddox22@users.noreply.github.com>
2022-04-21 11:42:27 -0400

    new file

---
+++ variablefontshit/script.js
@@ -0,0 +1,14 @@
+
+   var speed = 2800; 
+    var t = setInterval(function(){
+   var slideSource = document.getElementById('shadow');
+   slideSource.classList.toggle('fade');
+  }, speed);
+
+ // var speed = 2800; 
+//  var t = setInterval(function(){
+ //var slideSource = document.getElementById('looking-for-shade');
+ //slideSource.classList.toggle('fade');
+//}, speed);
+    
+ 
